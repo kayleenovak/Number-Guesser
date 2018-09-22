15a22032ba2384d73a73f251b25e0f7f6eda89f7
Kaylee Bunce <kaybunce@hotmail.com>
2018-09-22 12:55:41 -0600

    Refactor JS

--- Number-Guesser-Js.js
+++ Number-Guesser-Js.js
@@ -1,60 +1,26 @@
-var numberInput = document.querySelector('.number-input');
+var clearButton = document.querySelector('.clear');
 var guessButton = document.querySelector('.guess');
 var guessed = document.querySelector('.number-guess');
 var highOrLow = document.querySelector('.high-low');
-var clearButton = document.querySelector('.clear');
-var resetButton = document.querySelector('.reset');
 var lastGuess = document.querySelector('.last-guess');
-var minChange = document.querySelector('.min-change');
 var maxChange = document.querySelector('.max-change');
-var minNum = document.getElementById('minimum');
 var maxNum = document.getElementById('maximum');
-var rangeButton = document.querySelector('.range-button');
+var minChange = document.querySelector('.min-change');
+var minNum = document.getElementById('minimum');
+var numberInput = document.querySelector('.number-input');
 var randomNumber;
+var rangeButton = document.querySelector('.range-button');
+var resetButton = document.querySelector('.reset');
 
-function generateNumber(max, min) {
-  randomNumber = Math.floor(Math.random() * (max - min) + min);
-};
-
-function errorMessage () {
-  var numerical = parseInt(numberInput.value);
-  var min = parseInt(minNum.value);
-  var max = parseInt(maxNum.value);
-  if (numerical < min || numerical > max ) {
-      lastGuess.innerText = `Please choose a number between ${min} and ${max}`;
-      lastGuess.style.color = "#eb008b";
-      guessed.innerText = '';
-  } else {
-      lastGuess.innerText = 'Your Last Guess Was';
-      lastGuess.classList.add('last-guess-class');
-      lastGuess.style.color = '#000000';
-  }
-}  
-
-rangeButton.addEventListener('click', function() {
-  var min = parseInt(minNum.value);
-  var max = parseInt(maxNum.value);
-  var numerical = parseInt(numberInput.value);  
-  generateNumber(max, min);
-  errorMessage();
-  guessButton.disabled = false;
-  rangeButton.disabled = true;
-  resetButton.disabled = false;
-})
+clearButton.addEventListener('click', clearGuess)
+guessButton.addEventListener('click', guessNumber)
+numberInput.addEventListener('keyup', clearBtnDisable) 
+rangeButton.addEventListener('click', setMinMax);
+resetButton.addEventListener('click', resetGame)
 
 
-guessButton.addEventListener('click', function () {
-  var numberVal = numberInput.value;
-  var numerical = parseInt(numberInput.value);
-  var min = parseInt(minNum.value);
-  var max = parseInt(maxNum.value); 
-  guessed.innerText = numberVal;
-  
-  errorMessage();
-
-  resetButton.disabled = false;
-
-  if (numberVal == randomNumber) {
+function changeHighLowText(numberVal, numerical, min, max) {
+    if (numberVal == randomNumber) {
     highOrLow.innerText = 'BOOM!';
   } else if (numberVal < randomNumber) {
     highOrLow.innerText = 'That is too low!';
@@ -63,49 +29,128 @@ guessButton.addEventListener('click', function () {
   } else {
     highOrLow.innerText = 'That is too high!';
   }
+}
 
-  if(randomNumber == numberVal) {
-    var min = min - 10;
-    var max = max + 10;
-    minNum.value = `${min}`;
-    maxNum.value = `${max}`;
-    generateNumber(max, min);
-    minChange.innerText = 'Your minimum has been decreased by 10';
-    maxChange.innerText = 'Your maximum has been increased by 10';
-    rangeButton.disabled = true;
-  } else {
-    minChange.innerText = '';
-    maxChange.innerText = '';;
-  }
-});
-
-clearButton.addEventListener('click', function() {
-  numberInput.value = '';
-  clearButton.disabled = true;
-});
-
-resetButton.addEventListener('click', function () {
-  var min = parseInt(minNum.value);
-  var max = parseInt(maxNum.value);
+function changeMinMax(min,max) {
+  var min = min - 10;
+  var max = max + 10;
+  minNum.value = `${min}`;
+  maxNum.value = `${max}`;
   generateNumber(max, min);
-  numberInput.value = '';
-  guessed.innerText = '';
-  minNum.value = '';
-  maxNum.value = '';
-  highOrLow.innerText = 'Game has been reset!';
-  minChange.innerText = '';
-  maxChange.innerText = '';
-  lastGuess.innerText = '';
-  resetButton.disabled = true;
-  guessButton.disabled = true;
-  clearButton.disabled = true;
-  rangeButton.disabled = false;
-});
+  changeMinMaxText();
+  rangeButton.disabled = true;
+}
 
-numberInput.addEventListener('keyup', function() {
+function changeMinMaxText() {
+  minChange.innerText = 'Your minimum has been decreased by 10';
+  maxChange.innerText = 'Your maximum has been increased by 10';
+}
+
+function chooseNumClass(min, max){
+  lastGuess.innerText = `Please choose a number between ${min} and ${max}`;
+  lastGuess.style.color = "#eb008b";
+  guessed.innerText = '';
+}
+
+function clearBtnDisable() {
   if (numberInput.value.length === 0) {
     clearButton.disabled = true;
   } else {
     clearButton.disabled = false;
   }
-});
+}
+
+function clearGuess() {
+  numberInput.value = '';
+  clearButton.disabled = true;
+}
+
+function delegateClass(numerical, min, max) {
+  if (numerical < min || numerical > max ) {
+    chooseNumClass(min, max);
+  } else {
+    lastGuessClass();
+  }
+}
+
+function delegateMinMaxChange(randomNumber, numberVal, min, max) {
+  if(randomNumber == numberVal) {
+    changeMinMax(min, max);
+  } else {
+    minMaxChangeText();
+  }
+}
+
+function errorMessage() {
+  var numerical = parseInt(numberInput.value);
+  var min = parseInt(minNum.value);
+  var max = parseInt(maxNum.value);
+  delegateClass(numerical, min, max)
+}
+
+function generateNumber(max, min) {
+  randomNumber = Math.floor(Math.random() * (max - min) + min);
+};
+
+function guessNumber() {
+  var numberVal = numberInput.value;
+  var numerical = parseInt(numberInput.value);
+  var min = parseInt(minNum.value);
+  var max = parseInt(maxNum.value); 
+  guessed.innerText = numberVal;
+  resetButton.disabled = false;
+  errorMessage();
+  changeHighLowText(numberVal, numerical, min, max);
+  delegateMinMaxChange(randomNumber, numberVal, min, max)
+}
+
+function lastGuessClass() {
+  lastGuess.innerText = 'Your Last Guess Was';
+  lastGuess.classList.add('last-guess-class');
+  lastGuess.style.color = '#000000';
+} 
+
+function minMaxChangeText() {
+  minChange.innerText = '';
+  maxChange.innerText = '';
+}
+
+function rangeButtonDisable() {
+  guessButton.disabled = false;
+  rangeButton.disabled = true;
+  resetButton.disabled = false;
+} 
+
+function resetButtonDisable() {
+  resetButton.disabled = true;
+  guessButton.disabled = true;
+  clearButton.disabled = true;
+  rangeButton.disabled = false;
+}
+
+function resetGame() {
+  var min = parseInt(minNum.value);
+  var max = parseInt(maxNum.value);
+  highOrLow.innerText = 'Game has been reset!';
+  generateNumber(max, min);
+  minMaxChangeText();
+  resetButton();
+  resetText();
+}
+
+function resetText() {
+  numberInput.value = '';
+  guessed.innerText = '';
+  minNum.value = '';
+  maxNum.value = '';
+  lastGuess.innerText = '';
+}
+
+function setMinMax() {
+  var min = parseInt(minNum.value);
+  var max = parseInt(maxNum.value);
+  var numerical = parseInt(numberInput.value);  
+  generateNumber(max, min);
+  errorMessage();
+  rangeButtonDisable();
+}
